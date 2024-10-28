1f2470a34819d9e14fd3e63c3dc0c71e624e1dca
Laboratorio <laboratorio@udla-ec.int>
2024-10-28 09:28:40 -0500

    creacion de proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WilliamRamirez_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
